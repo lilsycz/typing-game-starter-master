24c0c2de9582ee0abaa66b8db26f38db670bee8a
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-05-06 00:04:41 +0200

    added all the functions

--- scripts/index.js
+++ scripts/index.js
@@ -40,3 +40,63 @@ let score = 0;
 
 //Initializing time
 let time = 10;
+
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+//Get random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+//Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+//When type in the correct word
+function checkInput() {
+  if (text.value === randomWord) {
+    updateScore();
+    addWordToDOM();
+    time += 5;
+    text.value = "";
+  }
+}
+
+//Game over
+function gameOver() {
+    endgameEl.style.display = "flex"; 
+};
+
+//Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time;
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    gameOver();
+  }
+}
+
+settingsBtn.addEventListener("click", function() {
+  settings.classList.toggle("hide");
+});
+
+settingsForm.addEventListener("change", function(e) {
+  if (e.target.value === "hard") {
+      time = 10;
+    } else if (e.target.value === "medium") {
+      time = 15;
+    } else {
+      time = 20;
+    }
+});
+
+const timeInterval = setInterval(updateTime, 1000);
+addWordToDOM();
+text.addEventListener("input", checkInput);
